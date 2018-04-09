1e48cceb6621a160c707b00b50e571b1b2bed186
Alex <alex@fihl.net>
2018-04-09 15:18:58 +0100

    Created the creat project

---
+++ project_management/test/project_management/test/creatAProjectFeature.java
@@ -0,0 +1,58 @@
+package project_management.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.List;
+
+import cucumber.api.DataTable;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class creatAProjectFeature {
+
+	@Given("^a worker is register in the company$")
+	public void aWorkerIsRegisterInTheCompany() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^a worker is logged in$")
+	public void aWorkerIsLoggedIn() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^the worker creat a project (\\d+)$")
+	public void theWorkerCreatAProject(int arg1) throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^the project is created$")
+	public void theProjectIsCreated() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^that no worker is logged in$")
+	public void thatNoWorkerIsLoggedIn() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^they try to creat a project (\\d+)$")
+	public void theyTryToCreatAProject(int arg1) throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^I get the error message \"([^\"]*)\"$")
+	public void iGetTheErrorMessage(String arg1) throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+}
--- project_management/use_case/creatAProject.feature
+++ project_management/use_case/creatAProject.feature
@@ -1,19 +1,16 @@
-#Feature: Creat a project
-#	Description: A worker creat a project for the company
-#	Actors: worker
-#	
-#Background:
-#	Given a worker is register in the company
-#	And a worker is logged in
-#	
-#Scenario: Creat a project
-#	When the worker creat a project 011001
-#	Then the project is created	
-#
-#Scenario: The worker is not logged in
-#	Given that no worker is logged in
-#	When they try to creat a project 011001
-#	Then I get the error message "No user is logged in"
-#	
-#
-#	
+Feature: Creat a project
+	Description: A worker creat a project for the company
+	Actors: worker
+	
+Background:
+	Given a worker is register in the company
+	And a worker is logged in
+	
+Scenario: Creat a project
+	When the worker creat a project 011001
+	Then the project is created	
+
+Scenario: The worker is not logged in
+	Given that no worker is logged in
+	When they try to creat a project 011001
+	Then I get the error message "No user is logged in"
