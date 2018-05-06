8d7effae1dcdfdbc4bd0b1661eb7c1b481539376
NGeertinger <s153140@student.dtu.dk>
2018-05-06 13:16:02 +0200

    Date

--- project_management/src/project_management/app/Main.java
+++ project_management/src/project_management/app/Main.java
@@ -5,6 +5,10 @@ import project_management.test.ErrorMessage;
 
 import java.util.Scanner;
 
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.List;
+
 // import project_management.app.Activity;
 // import project_management.app.Employee;
 // import project_management.app.ManagementTool;
@@ -23,7 +27,10 @@ public class Main {
 		int selector = 0;
 		String inputString = "";
 		double inputDouble = 0;
-
+		int temp1 = 0;
+		int temp2 = 0;
+		int temp3 = 0;
+		
 		boolean quit = false;
 		boolean back = false;
 
@@ -34,7 +41,6 @@ public class Main {
 		
 		while (quit == false) {
 			System.out.printf("1. Create project\n2. Select project\n3. Quit\n");
-
 
 			selector = reader.nextInt();
 
@@ -80,9 +86,11 @@ public class Main {
 									managementTool.selectActivity(inputString);
 
 									while (back == false) {
-										System.out.printf("1. Add used time\n2. Edit used time\n3. See remaining time\n");
+										System.out.printf("1. Add used time\n2. See used time\n3. See remaining time\n");
 										System.out.printf("4. Assign a worker to this activity\n");
-										System.out.printf("5. Remove activity\n6. Back\n");
+										System.out.printf("5. Set start date\n6. Set end date\n");
+										System.out.printf("7. See start and end date\n");
+										System.out.printf("8. Remove activity\n9. Back\n");
 
 										selector = reader.nextInt();
 
@@ -92,7 +100,7 @@ public class Main {
 												inputDouble = reader.nextDouble();
 												managementTool.addUsedTime(inputDouble);
 												break;
-											case 2: // mangler funktion
+											case 2: // See used time
 												System.out.printf("%d\n", managementTool.getUsedTime(managementTool.getEmployeeLoggedIn().getUsername()));
 												break;
 											case 3: // mangler funktion
@@ -107,11 +115,42 @@ public class Main {
 													System.out.println(e.getMessage());
 												}
 												break;
-											case 5: // Remove activity
+											case 5: // set start date
+												System.out.printf("Enter year\n");
+												temp1 = reader.nextInt();
+												System.out.printf("Enter month number\n");
+												temp2 = reader.nextInt();
+												System.out.printf("Enter day of month\n");
+												temp3 = reader.nextInt();
+												GregorianCalendar startDate = new GregorianCalendar(temp1, temp2, temp3);
+												managementTool.addActivityStartDate(startDate);
+												break;
+											case 6: // set end date
+												System.out.printf("Enter year\n");
+												temp1 = reader.nextInt();
+												System.out.printf("Enter month number\n");
+												temp2 = reader.nextInt();
+												System.out.printf("Enter day of month\n");
+												temp3 = reader.nextInt();
+												GregorianCalendar endDate = new GregorianCalendar(temp1, temp2, temp3);
+												try {
+													managementTool.addActivityEndDate(endDate);
+												} catch (startDateAfterEndDateException e) {
+													System.out.println(e.getMessage());
+												}
+												break;
+											case 7: // see start and end date
+												System.out.printf("\nStart date:\n");
+												System.out.println(managementTool.getActivityStartDate().getTime());
+												System.out.printf("End date:\n");
+												System.out.println(managementTool.getActivityEndDate().getTime());
+												System.out.printf("\n");
+												break;
+											case 8: // Remove activity
 												managementTool.deleteActivity();
 												back = true;
 												break;
-											case 6: // back
+											case 9: // back
 												back = true;
 												break;
 										}
@@ -169,7 +208,9 @@ public class Main {
 
 									while (back == false) {
 										System.out.printf("1. Add used time\n2. See used time\n3. See remaining time\n");
-										System.out.printf("4. Back\n");
+										System.out.printf("4. Set start date\n5. Set end date\n");
+										System.out.printf("6. See start and end date\n");
+										System.out.printf("7. Back\n");
 
 										selector = reader.nextInt();
 
@@ -185,7 +226,38 @@ public class Main {
 											case 3: // mangler funktion
 												System.out.printf("\nmangler\n");
 												break;
-											case 4: // Back
+											case 4: // set start date
+												System.out.printf("Enter year\n");
+												temp1 = reader.nextInt();
+												System.out.printf("Enter month number\n");
+												temp2 = reader.nextInt();
+												System.out.printf("Enter day of month\n");
+												temp3 = reader.nextInt();
+												GregorianCalendar startDate = new GregorianCalendar(temp1, temp2, temp3);
+												managementTool.addActivityStartDate(startDate);
+												break;
+											case 5: // set end date
+												System.out.printf("Enter year\n");
+												temp1 = reader.nextInt();
+												System.out.printf("Enter month number\n");
+												temp2 = reader.nextInt();
+												System.out.printf("Enter day of month\n");
+												temp3 = reader.nextInt();
+												GregorianCalendar endDate = new GregorianCalendar(temp1, temp2, temp3);
+												try {
+													managementTool.addActivityEndDate(endDate);
+												} catch (startDateAfterEndDateException e) {
+													System.out.println(e.getMessage());
+												}
+												break;
+											case 6: // see start and end date
+												System.out.printf("\nStart date:\n");
+												System.out.println(managementTool.getActivityStartDate().getTime());
+												System.out.printf("End date:\n");
+												System.out.println(managementTool.getActivityEndDate().getTime());
+												System.out.printf("\n");
+												break;
+											case 7: // Back
 												back = true;
 												break;
 										}
